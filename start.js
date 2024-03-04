4a4e65e34c9587d402b9b73c226ec519fef3e93e
Chandler Abel <93584687+abch21li@users.noreply.github.com>
2024-03-04 14:21:04 +0100

    Created page for stores/data is fetched via 'stores' button

--- start.js
+++ start.js
@@ -1,13 +1,20 @@
-fetch("http://localhost:3000/stores/all")
-.then((response) => {
-    return response.json();
-})
 
-.then((stores) => {
-    stores.forEach((d) => {
-        const para = document.createElement("p");
-        const node = document.createTextNode(d["name"]);
-        para.appendChild(node);
-        document.body.appendChild(para);
-    });
-});
+document.addEventListener('DOMContentLoaded', function() {
+      const storesLink = document.getElementById('stores-link');
+      const contentDiv = document.getElementById('content');
+
+      storesLink.addEventListener('click', function (event) {
+        event.preventDefault();
+
+        fetch('http://localhost:3000/stores/all')
+        .then((response) => response.json())
+        .then((stores) => {
+            let storesHTML = '<h2>Stores in Jönköping</h2';
+            stores.forEach((d) => {
+                storesHTML += `<p>${d['name']}</p>`
+            });
+            contentDiv.innerHTML = storesHTML;
+        });
+      });
+
+})
